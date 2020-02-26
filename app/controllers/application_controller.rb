2e77dd0da75fe48bd86d892b2c264e653359d8b6
dwinniford <dwinniford@gmail.com>
2020-02-26 07:41:10 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
+
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+  
 
   get '/' do
     erb :index
   end
+
+ 
 end
 
-# rspec spec/features/04_basic_view_spec.rb
+# rspec spec/features/05_song_form_spec.rb
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
 # Add seed data here. Seed your database with `rake db:seed`
-
+#LibraryParser.parse
+# Song.create(name: "Baby Beluga")
+#Song.create(name: "Three Little Pumpkins")
+raffi = Artist.create(name: "Raffi")
+raffi.songs.build(name: "Baby Beluga")
+raffi.songs.build(name: "Three Little Pumpkins")
+raffi.save 
+Genre.create(name: "childrens")
+Genre.create(name: "folk")
